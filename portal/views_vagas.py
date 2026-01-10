db059af8ccb28c337bc352a5218216ac7021a390
maurozacarias <maurozacariaspechisso@gmail.com>
2026-01-10 20:06:57 +0200

    Melhoria no blueprint de vagas , criacao do template nova para adicionar novas vagas pelas empresas

--- portal/views_vagas.py
+++ portal/views_vagas.py
@@ -13,20 +13,44 @@ from portal.models import Vaga
 from portal.decorators import role_required
 
 
-vaga_bp=Blueprint("vagas",__name__, template_folder ="template")
+vaga_bp=Blueprint("vagas",__name__, url_prefix="/vagas",template_folder="../templates/vagas")
 
-@vaga_bp.route("/vagas",methods=["POST","GET"])
+
+@vaga_bp.route("/nova", methods=["GET", "POST"])
 @login_required
 @role_required("empresa")
-def criar_vaga():
-    data = request.json
-    vaga = Vagas(
-        titulo=data["titulo"],
-        descricao=data["descricao"]
-    )
-    db.session.add(vaga)
-    db.session.commit()
-    return jsonify({"message":"Vaga criada com sucesso!"}),201
+def nova_vaga():
+
+    empresa = Empresa.query.filter_by(usuario_id=current_user.id).first()
+
+    if not empresa:
+        flash("Perfil de empresa não encontrado.", "danger")
+        return redirect(url_for("main.index"))
+
+    if request.method == "POST":
+        titulo = request.form["titulo"]
+        descricao = request.form["descricao"]
+        requisitos = request.form["requisitos"]
+        localizacao = request.form["localizacao"]
+        salario = request.form.get("salario")
+
+        vaga = Vaga(
+            titulo=titulo,
+            descricao=descricao,
+            requisitos=requisitos,
+            localizacao=localizacao,
+            salario=salario,
+            empresa_id=empresa.id
+        )
+
+        db.session.add(vaga)
+        db.session.commit()
+
+        flash("Vaga publicada com sucesso!", "success")
+        return redirect(url_for("main.index"))
+
+    return render_template("vagas/nova.html.j2")
+
 
 @vaga_bp.route("/vagas",methods=["GET"])
 def listar_vagas():
